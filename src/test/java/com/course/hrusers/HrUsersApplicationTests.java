335cae677945f44abd9f411b8a4b0f1ca6e4c8a4
Fernando Leis <fernandoleis@gmail.com>
2020-12-16 12:05:11 -0300

    Project hr-users created

---
+++ src/test/java/com/course/hrusers/HrUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.hrusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
