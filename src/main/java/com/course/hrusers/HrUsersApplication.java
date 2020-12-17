03d0926dcba4780c116c45aaaf2ddaca828afefc
Fernando Leis <fernandoleis@gmail.com>
2020-12-17 14:18:01 -0300

    Database seeding and Bcrypt password

--- src/main/java/com/course/hrusers/HrUsersApplication.java
+++ src/main/java/com/course/hrusers/HrUsersApplication.java
@@ -1,15 +1,23 @@
 package com.course.hrusers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUsersApplication {
-
+public class HrUsersApplication implements CommandLineRunner {
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     public static void main(String[] args) {
         SpringApplication.run(HrUsersApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        //System.out.println("BCryptPasswordEncoder = " + bCryptPasswordEncoder.encode("123456"));
+    }
 }
